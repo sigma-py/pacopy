57ef3cc024c97036db5a1719c2010af736e1eaf6
Nico Schlömer <nico.schloemer@gmail.com>
2018-08-18 11:05:41 +0200

    add mittelmann test problem

--- test/test_fenics_mittelmann.py
+++ test/test_fenics_mittelmann.py
@@ -4,36 +4,32 @@ import math
 
 import matplotlib.pyplot as plt
 from dolfin import (
-    IntervalMesh,
-    # UnitSquareMesh,
+    RectangleMesh,
     FunctionSpace,
     dx,
     assemble,
     dot,
     grad,
-    DirichletBC,
     TrialFunction,
     TestFunction,
     exp,
     Function,
     solve,
+    Point,
 )
 
 import pycont
 
 
-class Bratu(object):
+class Mittelmann(object):
     def __init__(self):
-        mesh = IntervalMesh(20, 0, 1)
-        # mesh = UnitSquareMesh(20, 20)
+        mesh = RectangleMesh(Point(-0.5, -0.5), Point(+0.5, +0.5), 20, 20)
 
         self.V = FunctionSpace(mesh, "Lagrange", 1)
 
-        self.bc = DirichletBC(self.V, 0.0, "on_boundary")
-
         u = TrialFunction(self.V)
         v = TestFunction(self.V)
-        self.a = assemble(-dot(grad(u), grad(v)) * dx)
+        self.a = assemble(dot(grad(u), grad(v)) * dx)
         self.m = assemble(u * v * dx)
         return
 
@@ -47,45 +43,39 @@ class Bratu(object):
         v = TestFunction(self.V)
         ufun = Function(self.V)
         ufun.vector()[:] = u
-        out = self.a * u + lmbda * assemble(exp(ufun) * v * dx)
-        DirichletBC(self.V, ufun, "on_boundary").apply(out)
+        out = self.a * u - 10 * assemble((ufun - lmbda * exp(ufun)) * v * dx)
         return out
 
     def df_dlmbda(self, u, lmbda):
         v = TestFunction(self.V)
         ufun = Function(self.V)
         ufun.vector()[:] = u
-        out = assemble(exp(ufun) * v * dx)
-        self.bc.apply(out)
+        out = 10 * assemble(exp(ufun) * v * dx)
         return out
 
     def jacobian_solver(self, u, lmbda, rhs):
         t = TrialFunction(self.V)
         v = TestFunction(self.V)
-        # from dolfin import Constant
-        # a = assemble(
-        #     -dot(grad(t), grad(v)) * dx + Constant(lmbda) * exp(u) * t * v * dx
-        # )
         ufun = Function(self.V)
         ufun.vector()[:] = u
-        a = self.a + lmbda * assemble(exp(ufun) * t * v * dx)
-        self.bc.apply(a)
+        a = self.a - 10 * assemble((1.0 - lmbda * exp(ufun)) * t * v * dx)
         x = Function(self.V)
         solve(a, x.vector(), rhs)
         return x.vector()
 
 
 def test_bratu_fenics():
-    problem = Bratu()
+    problem = Mittelmann()
     u0 = Function(problem.V).vector()
     lmbda0 = 0.0
 
     plt.ion()
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plt.axis("square")
+    # plt.axis("square")
     plt.xlabel("$\\lambda$")
     plt.ylabel("$||u||_2$")
+    plt.grid()
     lmbda_list = []
     values_list = []
     line1, = ax.plot(lmbda_list, values_list, "-", color="#1f77f4")
@@ -95,8 +85,8 @@ def test_bratu_fenics():
         line1.set_xdata(lmbda_list)
         values_list.append(math.sqrt(problem.inner(sol, sol)))
         line1.set_ydata(values_list)
-        ax.set_xlim(0.0, 10.0)
-        ax.set_ylim(0.0, 6.0)
+        ax.set_xlim(0.0, 0.5)
+        ax.set_ylim(0.0, 5.0)
         fig.canvas.draw()
         fig.canvas.flush_events()
         return
