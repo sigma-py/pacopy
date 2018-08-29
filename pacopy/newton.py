880d25ec0c631c52667905e266dcc76d7af89e45
Nico Schlömer <nico.schloemer@gmail.com>
2018-08-29 12:13:44 +0200

    small fix for newton

--- pacopy/newton.py
+++ pacopy/newton.py
@@ -16,10 +16,8 @@ def newton(f, jacobian_solver, norm2, u0, tol=1.0e-10, max_iter=20, verbose=True
         print("||F(u)|| = {:e}".format(nrm))
 
     k = 0
-    is_converged = False
     while k < max_iter:
         if nrm < tol:
-            is_converged = True
             break
         du = jacobian_solver(u, -fu)
         u += du
@@ -28,6 +26,8 @@ def newton(f, jacobian_solver, norm2, u0, tol=1.0e-10, max_iter=20, verbose=True
         k += 1
         if verbose:
             print("||F(u)|| = {:e}".format(nrm))
+
+    is_converged = nrm < tol
 
     if not is_converged:
         raise NewtonConvergenceError(
