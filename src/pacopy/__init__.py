0a35930cb1e504c042d00277546489eb2610fe37
Nico Schlömer <nico.schloemer@gmail.com>
2021-10-04 15:46:05 +0200

    remove __about__

--- src/pacopy/__init__.py
+++ src/pacopy/__init__.py
@@ -1,10 +1,8 @@
-from .__about__ import __version__
 from .branch_switching import branch_switching
 from .euler_newton import euler_newton
 from .natural import natural
 
 __all__ = [
-    "__version__",
     "natural",
     "euler_newton",
     "branch_switching",
